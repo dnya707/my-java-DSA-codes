5eee8d33e6091b43ceff906524e3a12935610d72
Dnyaneshwari <tondedb@gmail.com>
2025-11-26 11:43:58 -0500

    Add files via upload

---
+++ MarksLoop.java
@@ -0,0 +1,11 @@
+// import java.util.*;
+public class MarksLoop {
+    public static void main(String[] args) {
+        int marks[]={56,67,79,46};
+        System.out.println("The marks of the student is:");
+        for(int i=0;i<marks.length;i++)
+        {
+            System.out.println("marks is:" +(i+1)+":" +marks[i]);
+        }
+    }
+}
