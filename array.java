97d0e4035b893e5b646ebbc5ebaa17ad457dcddc
Dnyaneshwari <tondedb@gmail.com>
2025-11-28 02:16:03 -0500

    Add files via upload

---
+++ array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class dsa1{
+    static void show(int arr[]){
+  
+    Scanner sc= New Scanner(System.in);
+    System.out.println("Enter 4 numbers:");
+ arr[0]=sc.nextInt();
+ arr[1]=sc.nextInt();
+ arr[2]=sc.nextInt();
+ arr[3]=sc.nextInt();
+}
+public static void dsa1(String args[]){
+    int arr[]=new int;
+    input(arr);
+    show(arr);
+}
+}
