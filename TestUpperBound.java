11bceba5efa7cb1e5fda643d5a5cc25a242dbae1
Dnyaneshwari Tonde <tondedb@gmail.com>
2026-01-27 08:00:04 -0500

    Add files via upload

---
+++ TestUpperBound.java
@@ -0,0 +1,27 @@
+public class TestUpperBound {
+
+    // Lower Bound method
+    public static int upperBound(int[] array, int target) {
+        int low = 0;
+        int high = array.length;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (array[mid] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 4, 5, 6, 8};
+        int target = 4;
+
+        int lb = upperBound(arr, target);
+        System.out.println("upper bound of " + target + " is at index: " + lb);
+    }
+}
